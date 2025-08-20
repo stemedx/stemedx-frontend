c518f205f0198c171fa2d89d649ca93c601e0a80
Mahendra Bandara <mahendrabandara@Mahendras-MacBook-Pro.local>
2025-08-21 03:08:09 +0530

    add glassy navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 function Header() {
   return (
-    <div className="fixed">
-      <h1>Hello</h1>
-    </div>
+    <header className="fixed top-2 left-100 right-100 pt-2 pb-2 rounded-[10px] z-10 backdrop-blur-[1px] backdrop-brightness-75 bg-white/30">
+      <h1 className="text-[16px] font-normal">Your Title</h1>
+    </header>
   );
 }
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage:
+          "url('https://www.baltana.com/files/wallpapers-16/Abstract-Digital-Art-Best-4K-Wallpaper-41512.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}
+    >
       <Header />
     </div>
   );
